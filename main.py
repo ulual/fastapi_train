4ff143e49b1ad3cdbf9cf850ca39353576803202
ulual <ulualibunao@gmail.com>
2023-04-18 14:53:32 -0700

    2nd version of main.py (1:07:35)

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def root():
 
 
 
-
+# To look at a post
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
 
-
+# To create a post
 @app.post("/createposts")
 def create_posts(payload: dict = Body(...)):
     print(payload)
